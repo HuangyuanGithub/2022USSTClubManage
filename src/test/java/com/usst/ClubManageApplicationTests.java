67ef69a4d4255b194166e458a14dbae5a8fef14a
huangyuan <1031608160@qq.com>
2022-06-23 10:55:05 +0800

    初始化

---
+++ src/test/java/com/usst/ClubManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
